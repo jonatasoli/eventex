3ef2e9ea64084f07de6b993899359973c1227b83
Jonatas Oliveira <contato@jonatasoliveira.me>
2017-12-27 09:50:40 -0200

    persist subscriptions

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.template.loader import render_to_string
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
 
 def subscribe(request):
     if request.method == 'POST':
@@ -25,6 +26,8 @@ def create(request):
             'subscriptions/subscription_email.txt',
             form.cleaned_data)
 
+    Subscription.objects.create(**form.cleaned_data)
+    # Success feedback
     messages.success(request, 'Inscrição realizada com sucesso!')
 
     return HttpResponseRedirect('/inscricao/')
